144dd9309a906aa5cf26145da1d95bff2afea123
Eugene <gk16121983@gmail.com>
2020-02-06 21:03:55 +0200

    feature SecondMenu.tsx, SecondMenu.scss

---
+++ src/components/second-menu/SecondMenu.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './SecondMenu.scss'
+
+const SecondMenu: React.FunctionComponent = () => {
+    return (
+        <nav className="second-nav">
+            <ul className="second-nav__menu">
+               <ul className="second-nav__left-menu">
+                   <li className="second-nav__left-menu__item">
+                       <a href="/" className="second-nav__left-menu__link">
+                           All Projects (358)
+                       </a>
+                   </li>
+                   <li className="second-nav__left-menu__item">
+                       <a href="/" className="second-nav__left-menu__link">
+                           Workflow
+                       </a>
+                   </li>
+               </ul>
+                <ul className="second-nav__right-menu">
+                    <li className="second-nav__right-menu__item">
+                        <a href="/" className="second-nav__right-menu__link">
+                            Show projects:
+                        </a>
+                    </li>
+                    <li className="second-nav__right-menu__item">
+                        <a href="/" className="second-nav__right-menu__link">
+                            All
+                        </a>
+                    </li>
+                </ul>
+            </ul>
+        </nav>
+    )
+}
+
+export default SecondMenu
