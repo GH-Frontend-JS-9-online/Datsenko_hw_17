5f7ebe5148af8dd0afed762b1f3b613b9601c6e5
Eugene <gk16121983@gmail.com>
2020-02-06 21:01:26 +0200

    refactor App.tsx, App.scss

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,7 +3,7 @@ import './App.scss'
 import AppHeader from '../app-header/AppHeader'
 import Main from '../main/Main'
 
-const App = () => {
+const App: React.FunctionComponent = () => {
     return (
         <div className="App">
             <AppHeader/>
