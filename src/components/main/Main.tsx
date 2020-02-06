0504a47e029929bae80e9473fb0c81b9d8cf83b9
Eugene <gk16121983@gmail.com>
2020-02-06 21:32:46 +0200

    refactor Main.tsx

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,12 +1,14 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './Main.scss'
 import Aside from '../aside/Aside'
+import Middle from '../middle/Middle'
 
 const Main:React.FunctionComponent = () => {
     const asideMenuIcons: Array<string> = ['home', 'bars', 'chart-line', 'envelope', 'user-friends']
     return (
         <main className="main">
             <Aside asideMenuIcons={asideMenuIcons}/>
+            <Middle/>
         </main>
     )
 }
