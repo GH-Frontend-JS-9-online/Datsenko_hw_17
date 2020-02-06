451964ced02d3e7288a656e677f87e67a143c5d0
Eugene <gk16121983@gmail.com>
2020-02-06 21:01:53 +0200

    refactor AppHeader.tsx

--- src/components/app-header/AppHeader.tsx
+++ src/components/app-header/AppHeader.tsx
@@ -1,15 +1,14 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './AppHeader.scss'
 import TopMenu from '../top-menu/TopMenu'
-
 const logo = require('../../assets/images/header/logo.png')
 
-const AppHeader:React.FC = () => {
+const AppHeader:React.FunctionComponent = () => {
     const topMenuIcons: Array<string> = ['plus', 'search', 'bell', 'circle']
     return (
         <header className="header">
             <nav className="top-nav">
-                <a href="#" className="top-nav__logo">
+                <a href="/" className="top-nav__logo">
                     <img src={logo} alt="logo"/>
                 </a>
                 <TopMenu topMenuIcons={topMenuIcons}/>
