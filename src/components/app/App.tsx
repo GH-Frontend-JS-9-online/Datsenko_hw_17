cefae7d89a6c6a1657cc476b3c92a7808dcd4a1e
Eugene <gk16121983@gmail.com>
2020-02-06 12:42:49 +0200

    refactor App.tsx

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.scss'
 import AppHeader from '../app-header/AppHeader'
+import Main from '../main/Main'
 
 const App = () => {
     return (
         <div className="App">
             <AppHeader/>
+            <Main/>
         </div>
     )
 }
