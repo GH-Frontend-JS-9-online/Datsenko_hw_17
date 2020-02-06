fd52c4e1b4ed1024291fe7bbe92555c33b625bbb
Eugene <gk16121983@gmail.com>
2020-02-06 20:59:07 +0200

    feature Middle.tsx, Middle.scss

---
+++ src/components/middle/Middle.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Middle.scss'
+import SecondMenu from '../second-menu/SecondMenu'
+
+const Middle:React.FunctionComponent = () => {
+    return (
+        <div className="middle">
+        <SecondMenu/>
+
+        </div>
+    )
+}
+
+export default Middle
