7050f658a6a5faa849cf2416461e6bd64cf3b393
Eugene <gk16121983@gmail.com>
2020-02-07 18:00:39 +0200

    refactor Middle.tsx

--- src/components/middle/Middle.tsx
+++ src/components/middle/Middle.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Middle.scss'
 import SecondMenu from '../second-menu/SecondMenu'
+import ProjectsItem from '../projects-item/ProjectsItem'
 
 const Middle:React.FunctionComponent = () => {
     return (
         <div className="middle">
-        <SecondMenu/>
-
+            <SecondMenu/>
+            <ProjectsItem projectsItems={[...JSON.parse(localStorage.getItem('projectsPayloads') as any)]} />
         </div>
     )
 }
-
 export default Middle
