982e2510d802d9cac38c825260591b831ecfe220
Eugene <gk16121983@gmail.com>
2020-02-07 21:10:52 +0200

    refactor Middle.tsx

--- src/components/middle/Middle.tsx
+++ src/components/middle/Middle.tsx
@@ -1,12 +1,24 @@
 import React, {useState} from 'react'
 import './Middle.scss'
 import SecondMenu from '../second-menu/SecondMenu'
+import ProjectsMenu from '../project-menu/ProjectsMenu'
 import ProjectsItem from '../projects-item/ProjectsItem'
 
 const Middle:React.FunctionComponent = () => {
+    const projectMenuItems: Array<string> = [
+        'Project title',
+        'Value',
+        'Deadline',
+        'Time spent',
+        'Progress',
+        'Status',
+        'Assigned to',
+        ''
+    ]
     return (
         <div className="middle">
             <SecondMenu amountProjects={[...JSON.parse(localStorage.getItem('projectsPayloads') as any)].length}/>
+            <ProjectsMenu projectMenuItems={projectMenuItems}/>
             <ProjectsItem projectsItems={[...JSON.parse(localStorage.getItem('projectsPayloads') as any)]} />
         </div>
     )
