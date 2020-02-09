fecc2440e51d90f43a1344a23fb476bdfba3c8ef
Eugene <gk16121983@gmail.com>
2020-02-09 17:49:41 +0200

    refactor Middle.tsx

--- src/components/middle/Middle.tsx
+++ src/components/middle/Middle.tsx
@@ -3,8 +3,15 @@ import './Middle.scss'
 import SecondMenu from '../second-menu/SecondMenu'
 import ProjectsMenu from '../project-menu/ProjectsMenu'
 import ProjectsItem from '../projects-item/ProjectsItem'
+import DashboardApiServices from "../../services/DashboardServices";
+import Project from "../../services/Project";
 
 const Middle:React.FunctionComponent = () => {
+    let svc = new DashboardApiServices()
+    svc.getProjectsInfo(JSON.parse(localStorage.getItem('token') as any) ,(projects: Project) => {
+        localStorage.setItem('projects', JSON.stringify(projects))
+        console.table(projects)
+    })
     const projectMenuItems: Array<string> = [
         'Project title',
         'Value',
@@ -17,9 +24,9 @@ const Middle:React.FunctionComponent = () => {
     ]
     return (
         <div className="middle">
-            <SecondMenu amountProjects={[...JSON.parse(localStorage.getItem('projectsPayloads') as any)].length}/>
+            <SecondMenu amountProjects={[...JSON.parse(localStorage.getItem('projects') as any).projects].length}/>
             <ProjectsMenu projectMenuItems={projectMenuItems}/>
-            <ProjectsItem projectsItems={[...JSON.parse(localStorage.getItem('projectsPayloads') as any)]} />
+            <ProjectsItem projectsItems={[...JSON.parse(localStorage.getItem('projects') as any).projects]} />
         </div>
     )
 }
