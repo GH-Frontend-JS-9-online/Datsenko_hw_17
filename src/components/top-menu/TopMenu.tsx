a0d12683f8ac47c6f66e584f48f92dfecace553c
Eugene <gk16121983@gmail.com>
2020-02-10 21:39:11 +0200

    refactor TopMenu.tsx

--- src/components/top-menu/TopMenu.tsx
+++ src/components/top-menu/TopMenu.tsx
@@ -1,23 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './TopMenu.scss'
+import AddProject from '../add-project/AddProject'
 
 interface TopMenuIconsProps {
     topMenuIcons: Array<string>
 }
 
 const TopMenu: React.FunctionComponent<TopMenuIconsProps> = props => {
+    const [isShowProject, setShowProject] = useState(false)
+    const toggleHandler = (id: number):void =>  {
+        if (id === 0) {
+            setShowProject(!isShowProject)
+        }
+    }
     return (
-        <ul className="top-nav__menu">
-            {props.topMenuIcons.map((item, index) => {
-              return (
-                  <li key={index} className="top-nav__menu__item">
-                      <a href="/" className="top-nav__menu__link" >
-                          <i className={`fas fa-${item}`}></i>
-                      </a>
-                  </li>
-              )
-            })}
-        </ul>
+        <>
+            <ul className="top-nav__menu">
+                {props.topMenuIcons.map((item, index) => {
+                  return (
+                      <li key={index} onClick={(event) => toggleHandler(index)} className="top-nav__menu__item">
+                          <a href="#" className="top-nav__menu__link" >
+                              <i className={`fas fa-${item}`}></i>
+                          </a>
+                      </li>
+                  )
+                })}
+            </ul>
+            <AddProject addProjectDisplay={(isShowProject) ? 'block' : 'none'}/>
+         </>
     )
 }
 
